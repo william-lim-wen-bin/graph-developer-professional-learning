ce7bdf9e23ef89eb009d8b534a4df76ba853eccf
william-lim-wen-bin <132965638+william-lim-wen-bin@users.noreply.github.com>
2024-09-25 12:07:36 +0800

    [part 1] Referencing an entity

--- odyssey-voyage-I-main/subgraph-reviews/resolvers.js
+++ odyssey-voyage-I-main/subgraph-reviews/resolvers.js
@@ -1,15 +1,26 @@
 const resolvers = {
   Query: {
-    latestReviews: (_, __, {dataSources}) => {
+    latestReviews: (_, __, { dataSources }) => {
       return dataSources.reviewsAPI.getLatestReviews();
-    }
+    },
   },
   Mutation: {
-    submitReview: (_, {locationReview}, {dataSources}) => {
-      const newReview = dataSources.reviewsAPI.submitReviewForLocation(locationReview);
-      return {code: 200, success: true, message: 'success', locationReview: newReview};
-    }
-  }
+    submitReview: (_, { locationReview }, { dataSources }) => {
+      const newReview =
+        dataSources.reviewsAPI.submitReviewForLocation(locationReview);
+      return {
+        code: 200,
+        success: true,
+        message: "success",
+        locationReview: newReview,
+      };
+    },
+  },
+  Review: {
+    location: ({ locationId }) => {
+      return { id: locationId };
+    },
+  },
 };
 
 module.exports = resolvers;
